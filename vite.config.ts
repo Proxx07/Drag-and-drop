b0a5d3905c1fe20dbd318c080d2bcb005e35fae3
Proxx07 <zakir.pulatov97@gmail.com>
2024-02-07 13:34:55 +0500

    GH deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: './',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
